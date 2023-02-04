8822f98c8a8c73767ba5e7ca8a3c760c177e6d6c
Ulisses76 <aulissesn@gmail.com>
2023-02-04 11:27:06 -0300

    almost done !!!

--- hangman.rb
+++ hangman.rb
@@ -1,16 +1,89 @@
-module GamingSets  
+module GamingSets
+  class GamingBody
+    def initialize(hidden_word, shots = 10, word_working)
+      @@hidden_word = hidden_word
+      @@word_working = word_working
+      @@shots = shots      
+      while @@word_working != @@hidden_word
+        showgame
+        print "\ttype a word: (or type 'save' to save the game) :"
+        choice = ""      
+        while !choice.match(/[a-z]/i)
+          choice = gets.chomp
+          if choice.downcase == "save"
+            save_game          
+          end
+          if @@word_working.include?(choice)
+            puts "\nalready typed !! try another one"
+            choice = ""
+          end
+          if choice.size > 1
+            puts "Wrong entry, try again"
+            choice = ""
+          end        
+        end
+
+        if @@hidden_word.include?(choice)
+          set_hidden(choice)
+        end
+        if @@hidden_word == @@word_working
+          congratz
+        end
+      end
+    end
+    
+    def congratz
+      puts ""
+      puts "############################################################"
+      puts "Congratz !!!!! Right asnwer !!!!\t'#{@@hidden_word.upcase}'"
+      puts "############################################################"
+      puts ""
+      
+      play_again
+    end
+
+    def play_again
+      puts "Do you want play again? (y or n)"
+      a = gets.chomp.downcase
+      if a == "y"
+        GameStart.new
+      end
+      
+      exit!
+    end      
+      
+    def set_hidden(choice)
+      mat = @@hidden_word.split("")
+      mat.each_with_index do |char,index|
+        if char == choice
+          @@word_working[index] = choice
+        end
+      end
+    end
+
+    def save_game
+      exit!
+    end
+
+    def showgame
+      print "\t"
+      @@word_working.each_char { |c| print c + " "}
+      print "\t\t\t try: #{@@shots} \n"      
+    end
+
+  end
+end
 
   class Gaming
-    
     def initialize
-      @@hidden_word = choice_word      
+      @@hidden_word = choice_word
     end
     
     def self.hidden_word
       @@hidden_word
     end
     
-    def choice_word      
+    def choice_word
       file = File.open("lib/google-10000-english-no-swears.txt")
       word_array = file.readlines
       word = ""
@@ -18,14 +91,9 @@ module GamingSets
       while word.size < 5 || word.size > 12 do
         word = word_array[rand(word_array.size)][0..-2]
       end
-
       return word
     end
-
   end
-
-end
-
 
 class GameStart
   include GamingSets
@@ -33,13 +101,17 @@ class GameStart
   def initialize
     a = welcome
     if a == ""
-      p Gaming.new.hidden_word
+      Gaming.new
+      a = "-" * Gaming.hidden_word.size
+      GamingBody.new(Gaming.hidden_word, 10, a)
     else
-      Load_game
-    end
-    
+      load_game
+    end    
   end
 
+  def load_game
+    exit!
+  end
 
   def welcome
     puts "
